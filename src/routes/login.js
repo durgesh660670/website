66f77cc28613c9b13124f179489b5302da82dc8b
Durgesh <ddgupta000@gmail.com>
2022-02-07 09:44:30 +0530

    end of v1.0

--- src/routes/login.js
+++ src/routes/login.js
@@ -10,7 +10,22 @@ const routes = express.Router()
 routes.get('/login', async (req, res) => {
 
     const details = await Details.findOne({ _id: '61f4d023c4665a0c6915c00d' })
-    res.render("login",{details:details});
+    res.render("login", { details: details });
+})
+routes.post('/process_login_form', async (req, res) => {
+    console.log("inside process_login_form")
+    const details = await Details.findOne({ _id: '61f4d023c4665a0c6915c00d' })
+    console.log(details);
+
+    let { email, password } = req.body
+    let email1 = 'admin@gmail.com'
+    let password1 = '123'
+    if (email1 === email && password1 === password) {
+        res.render("admin_user", { details: details })
+    }
+    else {
+        res.render("login", { details: details });
+    }
 })
 
 
